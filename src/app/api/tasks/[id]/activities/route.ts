96820e7ebe3f216d6ca4dadfa44cf96be11f83eb
Henri Okayama <henri.okayama@gmail.com>
2025-07-03 21:06:26 -0300

    chore: remove unused TaskActivity import

--- src/app/api/tasks/[id]/activities/route.ts
+++ src/app/api/tasks/[id]/activities/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { TaskActivity } from "@prisma/client";
 
 const handleServerError = (error: unknown) => {
   // console.error("Server error:", error);
