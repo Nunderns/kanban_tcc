87c7710859cdd5ffc225b6985c7e166e00e14c04
Henri Okayama <henri.okayama@gmail.com>
2025-08-02 09:14:12 -0300

    fix: correção

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth/next";
 
-// Utilize a mesma configuração de autenticação em todas as rotas
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
