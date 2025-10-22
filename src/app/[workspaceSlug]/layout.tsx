ad51e36afaecca4ed8450f191db34e55f4f23f34
Henri Okayama <67149267+Nunderns@users.noreply.github.com>
2025-10-22 08:40:45 -0300

    Make sidebar responsive

--- src/app/[workspaceSlug]/layout.tsx
+++ src/app/[workspaceSlug]/layout.tsx
@@ -18,7 +18,7 @@ export default async function WorkspaceLayout({
   }
 
 return (
-  <div className="flex h-screen bg-gray-50 dark:bg-gray-900">
+  <div className="flex min-h-screen flex-col bg-gray-50 dark:bg-gray-900 lg:h-screen lg:flex-row">
     <Sidebar workspaceSlug={workspaceSlug} />
     <main className="flex-1 overflow-y-auto p-0">{children}</main>
   </div>
